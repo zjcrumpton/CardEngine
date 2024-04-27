3cf61a526c9eef221e099b021251249db6f04593
zjcrumpton <zjcrumpton@gmail.com>
2024-04-26 23:27:52 -0600

    stack

--- src/poker/player.rs
+++ src/poker/player.rs
@@ -2,21 +2,43 @@ use crate::card::card::Card;
 
 pub struct Player {
     hand: Option<[Card; 2]>,
+    stack: u32,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum StackError {
+    InsufficientFunds(u32),
 }
 
 impl Player {
-    pub fn new() -> Self {
+    pub fn new(starting_stack: u32) -> Self {
         let hand: Option<[Card; 2]> = None;
 
-        Player { hand }
+        Player {
+            hand,
+            stack: starting_stack,
+        }
     }
 
     pub fn hand(&self) -> Option<&[Card; 2]> {
         self.hand.as_ref()
     }
 
+    pub fn stack(&self) -> u32 {
+        self.stack
+    }
+
     pub fn assign_hand(&mut self, new_hand: [Card; 2]) {
         self.hand = Some(new_hand);
+    }
+
+    pub fn decrease_stack(&mut self, amount: u32) -> Result<u32, StackError> {
+        if amount > self.stack {
+            Err(StackError::InsufficientFunds(amount - self.stack))
+        } else {
+            self.stack -= amount;
+            Ok(self.stack)
+        }
     }
 }
 
@@ -24,18 +46,18 @@ impl Player {
 mod tests {
     use crate::{
         card::{card::Card, rank::Rank, suit::Suit},
-        poker::player::Player,
+        poker::player::{Player, StackError},
     };
 
     #[test]
     fn starts_with_empty_hand() {
-        let player: Player = Player::new();
+        let player: Player = Player::new(0);
         assert_eq!(player.hand(), None);
     }
 
     #[test]
     fn can_be_assigned_a_hand() {
-        let mut player: Player = Player::new();
+        let mut player: Player = Player::new(0);
         let first_card: Card = Card {
             rank: Rank::Ace,
             suit: Suit::Spades,
@@ -49,5 +71,36 @@ mod tests {
         player.assign_hand(new_hand);
 
         assert_eq!(player.hand().unwrap(), &new_hand);
+    }
+
+    #[test]
+    fn assigns_initial_starting_stack() {
+        let player: Player = Player::new(1000);
+
+        assert_eq!(player.stack(), 1000);
+    }
+
+    #[test]
+    fn can_decrease_stack() {
+        let mut player: Player = Player::new(1000);
+
+        match player.decrease_stack(500) {
+            Ok(new_amount) => assert_eq!(new_amount, 500),
+            Err(_) => panic!("Did not expect an error result"),
+        }
+
+        assert_eq!(player.stack(), 500);
+    }
+
+    #[test]
+    fn can_not_go_negative() {
+        let mut player: Player = Player::new(1000);
+
+        match player.decrease_stack(2000) {
+            Ok(_) => panic!("Expected an error result"),
+            Err(StackError::InsufficientFunds(short)) => assert_eq!(short, 1000),
+        }
+
+        assert_eq!(player.stack(), 1000);
     }
 }
